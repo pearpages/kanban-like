191e536428c8b4d204db9922a5105efdbedbc2f7
pearpages <pere@soms.cat>
2017-06-14 17:26:49 +0200

    using altcontainer

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
+import AltContainer from 'alt-container';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions';
 import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = NoteStore.getState();
-    }
-
-    componentDidMount() {
-        NoteStore.listen(this.storeChanged);
-    }
-
-    componentWillUnmount() {
-        NoteStore.unlisten(this.storeChanged);
-    }
-
-    storeChanged = (state) => {
-        this.setState(state);
+    render() {
+        return (
+            <div>
+                <button onClick={this.addNote}>+</button>
+                <AltContainer
+                    stores={[NoteStore]}
+                    inject={{
+                        notes: () => NoteStore.getState().notes
+                    }}
+                >
+                    <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+                </AltContainer>
+            </div>
+        );
     }
 
     addNote = () => {
-        NoteActions.create({task: 'New task'});
+        NoteActions.create({ task: 'New task' });
     };
 
     editNote = (id, task) => {
@@ -33,26 +32,12 @@ export default class App extends React.Component {
             return;
         }
 
-        NoteActions.update({id,task});
+        NoteActions.update({ id, task });
     };
 
-    deleteNote = (id,e) => {
+    deleteNote = (id, e) => {
         e.stopPropagation();
 
         NoteActions.delete(id);
-    }
-
-    render() {
-        const notes = this.state.notes;
-        return (
-            <div>
-                <button onClick={this.addNote}>+</button>
-                <Notes
-                    notes={notes}
-                    onEdit={this.editNote}
-                    onDelete={this.deleteNote}
-                />
-            </div>
-        );
     }
 }
